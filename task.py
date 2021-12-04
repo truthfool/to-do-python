d26c8848d4215a5bdbd1ac6b6373b9afb0d86595
shani <ishanranasingh@gmail.com>
2021-12-04 17:28:13 +0530

    changes in tasks

--- task.py
+++ task.py
@@ -23,6 +23,7 @@ def list_tasks():
     for task in file1:
         print("{}. {}[{}]".format(i,task[:-2],task.split()[-1]))
         i+=1
+
 def list_tasks_completed():
     file1 = open("completed.txt","r").readlines()
     i=1
@@ -31,15 +32,13 @@ def list_tasks_completed():
         i+=1
 
 def report():
-    file1=open("task.txt","r").read()
-    pending = file1.split("\n")
-    file2=open("completed.txt","r").read()
-    completed = file2.split("\n")
+    file1=open("task.txt","r").readlines()
+    file2=open("completed.txt","r").readlines()
     count1,count2=0,0
-    for i in pending:
+    for i in file1:
         if i:
             count1+=1
-    for i in completed:
+    for i in file2:
         if i:
             count2+=1
 
@@ -51,57 +50,88 @@ def report():
 
 def add(next_words):
     priority=int(next_words[0])
-    task=str(next_words[1:])
-    with open("task.txt","r+") as f:
-        lines=f.readlines()
-        for line in lines:
-            if int(line[-1])>priority:
-                lines[i] = lines[i].strip() + '2012\n'
-                f.seek(0)
-                for line in lines:
-                    f.write(line)
+    task=str(next_words[2:])
+    
+    with open("task.txt","r") as f:
+        lines=f.read().splitlines()
+    
+    i=0
+    print(lines)
+    for line in lines:
+        print(line[-1])    
+        if int(line[-1])<=int(priority):
+            i+=1
+        else:
+            lines.insert(i, task+" "+str(priority))
 
-def delete(val):
-    s=""
-    with open("task.txt", "r") as f:
-        lines = f.readlines()
-        if lines[-1]==val:
-            s=lines[:-2]
-
+    i=0
     with open("task.txt", "w") as f:
         for line in lines:
-            if line.strip("\n") != s:
+            if i<len(lines)-1:
+                f.write(line+"\n")
+                i+=1
+            else:
                 f.write(line)
+
+
+
 def done(val):
-    s=""
     with open("task.txt", "r") as f:
         lines = f.readlines()
-        if lines[-1]==val:
-            s=lines[:-2]
 
+    if int(val)>len(lines):
+        print("Error: no incomplete item with index {} exists.".format(val))
+        return
+
+    i,s=1,""
     with open("task.txt", "w") as f:
         for line in lines:
-            if line.strip("\n") != s:
+            if i!=int(val):
                 f.write(line)
             else:
-                add(s)
+                s=line
+                print("Marked item as done.")
+            i+=1
+    
+    with open("completed.txt","a+") as f:
+        f.write("\n"+s)
+
+def delete(val):
+    with open("task.txt", "r") as f:
+        lines = f.readlines()
+
+    if int(val)>len(lines):
+        print("Error: item with index {} does not exist. Nothing deleted.".format(val))
+        return
+
+    i=1
+    with open("task.txt", "w") as f:
+        for line in lines:
+            if i!=int(val):
+                f.write(line)
+            else:
+                print("Deleted item with index {}".format(val))
+            i+=1
 
 def task(a):
-    first_word = a.split()[0]
-    next_words=a[1:]
+    first_word = a[1]
+    next_words=""
+    
+    for i in a[2:]:
+        next_words+=i+" "
 
-    if a=="help":
+    if first_word=="help":
         help_task()
-    elif a=="ls":
+    elif first_word=="ls":
         list_tasks()
-    elif a=="add":
+    elif first_word=="add":
         add(next_words)
-    elif a=="del":
+    elif first_word=="del":
         delete(a[-1])
-    elif a=="done":
+    elif first_word=="done":
         done(a[-1])
-    elif a=="report":
+    elif first_word=="report":
         report()
 
 if __name__== "__main__":
-    task(sys.argv[1])
+    task(sys.argv)
